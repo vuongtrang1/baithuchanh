c29c2988c8979cbfc5bed24d4b37b2a92b8cc4eb
vuongtrang1 <97450410+vuongtrang1@users.noreply.github.com>
2022-01-17 18:22:10 +0700

    BaiThucHanh

---
+++ BaiThucHanh1/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String strHoTen = "";
+        System.out.println("Nhap ho ten");
+        strHoTen = sc.nextLine();
+        System.out.println("Xin Chao " + strHoTen);
+    }
+}
